92b5d816a4ec3a6f4a6f9fc43e656cfea19eb5c2
DirecTion1024 <fx18358196136@gmail.com>
2017-08-28 12:34:56 +0800

    add delete

---
+++ src/test/java/MyBatis/MyBatis/TestDelete.java
@@ -0,0 +1,37 @@
+package MyBatis.MyBatis;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class TestDelete{
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader;
+
+    static{
+        try{
+            reader    = Resources.getResourceAsReader("Configuration.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession(){
+        return sqlSessionFactory;
+    }
+   
+    public static void main(String[] args) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+        	session.delete("MyBatis.MyBatis.UserMapper.deleteById",3);
+        	session.commit();
+        } finally {
+        session.close();
+        }
+    }
+}
